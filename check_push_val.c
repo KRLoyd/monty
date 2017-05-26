6048776957f6a11c73b3343a426d6909adb5de4f
KRLoyd <loydkristen@gmail.com>
2017-05-26 21:41:37 +0000

    Create check_push_val.c

---
+++ check_push_val.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+/**
+ * check_push_val - checks push_val
+ * @push_val: integer to be evaluated
+ *
+ * Description: checks if push_val exists and is an integer
+ */
+void check_push_val(int push_val, unsigned int line_number)
+{
+
+	/* check that push_val is an integer */
+	printf("check_push: before _isdigit check\n");
+	if ((_isdigit(push_val) == 0))
+	{
+		printf("L%d: usage: push integer\n", line_number);
+		globals->err_val = EXIT_FAILURE;
+	}
+}
